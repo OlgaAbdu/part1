f6255ab355508a3c23d5ff5f8f45ad00238e3dd9
OlgaAbdu <olgasavik84@gmail.com>
2023-03-05 18:39:23 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const Header = (props) => {
+  console.log(props)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Welcome to {props.course} training</h1>
+    </div>
+  )
+}
+const Content = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <p>At your training you will learn : <br />1 : {props.part1}. That will include {props.exercise1} tasks.<br />
+        2 : {props.part2}. That will include {props.exercise2} tasks.<br />
+        3 : {props.part3}. That will include {props.exercise3} tasks.</p>
+    </div>
+  )
+}
+const Total = ( props ) => {
+  return (
+    <div>
+      <p>
+        After completing a total of {props.totalInAll} tasks, you can gain your
+        "Complete React in Action" certificate.
+      </p>
     </div>
   );
+};
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const part2 = 'Using props to pass data'
+  const part3 = 'State of component'
+  const exercise1 = 10;
+  const exercise2 = 7;
+  const exercise3 = 14;
+  const totalInAll = 0;
+  return (
+    <div>
+      <Header course={course} />
+      <Content part1={part1}
+        exercise1={exercise1}
+        part2={part2}
+        exercise2={exercise2}
+        part3={part3}
+        exercise3={exercise3} />
+      <Total totalInAll={10+7+14+0} />
+    </div>
+  )
 }
-
 export default App;
+
+
+
+
